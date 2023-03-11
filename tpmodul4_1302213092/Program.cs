36f199f69166703a33fd13bce0eab700b77ddbab
hifahir <putratertuahirzi@gmail.com>
2023-03-11 23:58:53 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_1302213092/Program.cs
+++ tpmodul4_1302213092/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+public class Program
+{
+    public static int getKodePos(String kelurahan, int kode)
+    {
+        string[] kelurahan = { "Batunuggal", "Kujangsari", "Mengger", "Wates", "Cijaura", "Jatisari", "Margasari", "Sakejati", "Kebonwaru", "Maleer", "Samoja" };
+        int[] kode = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+    }
+}
